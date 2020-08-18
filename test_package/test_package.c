7cb8369811e86c928d1c3f85c718a3d023068330
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-18 21:55:12 +0200

    timeout in test_package

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -3,13 +3,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifdef _MSC_VER
+#include <winsock2.h>
+#endif
+
 int main() {
   printf("hiredis version: %i.%i.%i\n", HIREDIS_MAJOR, HIREDIS_MINOR, HIREDIS_PATCH);
 
   const char *hostname = "127.0.0.1";
   int port = 6379;
+  struct timeval timeout = {3, 0};
 
-  redisContext *c = redisConnect(hostname, port);
+  redisContext *c = redisConnectWithTimeout(hostname, port, timeout);
   if (c == NULL) {
     printf("Error: Can't allocate redis context\n");
     return EXIT_FAILURE;
